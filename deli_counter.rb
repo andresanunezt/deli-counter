603b5a55a09274002d600750f8e423831fcd943e
Andrés Nuñez <andresanunez98@gmail.com>
2021-02-25 12:14:31 -0500

    commit tree 019cb464e5c7b08c9532eb15d559a7ff4d7e7107

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,30 @@
 # Write your code here.
+# require 'pry'
+def line(array)
+  if array.length == 0
+       puts  "The line is currently empty."
+  else
+      current_line = "The line is currently:"
+       array.each.with_index(1) do |custie_name,place_in_line| 
+       #  binding.pry
+      current_line <<  " #{place_in_line}. #{custie_name}"
+       end
+       puts current_line
+  end   
+end
+
+def take_a_number(array,custie)
+array << custie
+puts "Welcome, #{custie}. You are number #{array.length} in line."
+end
+
+def now_serving(people)
+       if people.empty?
+             puts "There is nobody waiting to be served!"
+       else  
+             removed_name = people.shift
+       #       binding.pry   
+            puts  "Currently serving #{removed_name}." 
+       end
+       
+end
